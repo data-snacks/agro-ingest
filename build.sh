4c3b886bdd06bc5cb819a14d6ac52254ac0d3003
jino.michel.aque <contact@geeklogbook.com>
2023-11-22 00:47:15 -0300

    add build sh

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-echo hello
+docker network create -d bridge demo_docker
+docker compose -f ./minio/docker-compose.yaml up -d
+docker compose -f ./sftp/docker-compose.yaml up -d
+read -p "Press any key..."
