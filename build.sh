4775a7d91f07a7f600072c81a07555ff44049dea
jino.michel.aque <contact@geeklogbook.com>
2023-11-22 17:49:05 -0300

    Add airbyte

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 docker network create -d bridge demo_docker
 docker compose -f ./minio/docker-compose.yaml up -d
 docker compose -f ./sftp/docker-compose.yaml up -d
+docker compose -f ./airbyte/docker-compose.yaml up -d
 read -p "Press any key..."
