5b757f102894b1f79d32ee16cee48c5512f7503c
jino.michel.aque <contact@geeklogbook.com>
2023-12-05 16:02:38 -0300

    add postgress

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ docker network create -d bridge demo_docker
 docker compose -f ./minio/docker-compose.yaml up -d
 docker compose -f ./sftp/docker-compose.yaml up -d
 docker compose -f ./airbyte/docker-compose.yaml up -d
+docker compose -f ./postgres/docker-compose.yaml up -d
 read -p "Press any key..."
