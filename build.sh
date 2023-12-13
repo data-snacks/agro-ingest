c48c3fa4383a0a7b540945a87877eeca290dfd03
jino.michel.aque <contact@geeklogbook.com>
2023-12-13 19:42:49 -0300

    add dagster

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ docker compose -f ./minio/docker-compose.yaml up -d
 docker compose -f ./sftp/docker-compose.yaml up -d
 docker compose -f ./airbyte/docker-compose.yaml up -d
 docker compose -f ./postgres/docker-compose.yaml up -d
+docker compose -f ./dagster/docker-compose.yml up -d
 read -p "Press any key..."
